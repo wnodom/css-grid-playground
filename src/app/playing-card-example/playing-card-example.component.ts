3ccc64103bfdcaf4b5a730ceb1443fa538877bde
Bill Odom <bill@billodom.com>
2018-05-05 10:24:44 -0500

    Show all cards across all suits

--- src/app/playing-card-example/playing-card-example.component.ts
+++ src/app/playing-card-example/playing-card-example.component.ts
@@ -9,6 +9,16 @@ export class PlayingCardExampleComponent implements OnInit {
 
   static label: string = 'Playing Card';
 
+  suits: string[] = [
+    'spades',
+    'hearts',
+    'clubs',
+    'diamonds',
+  ];
+
+  ranks: string[] =
+    [ 'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K' ];
+
   constructor() { }
 
   ngOnInit() {
